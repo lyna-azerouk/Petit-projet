7e2168b58980d61d5a58e99b498165054ac09427
Lynadjdjdjd <114083371+Lynadjdjdjd@users.noreply.github.com>
2023-03-16 16:06:23 +0100

    fichier csv

--- src/test/test.java
+++ src/test/test.java
@@ -1,15 +1,15 @@
-package test;
+package algorithms;
 
 import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-import algorithms.DefaultTeam;
-import algorithms.Naif;
 
 public class test {
 	protected static String samplesDirName = "../Samples";
@@ -20,9 +20,11 @@ public class test {
 	Naif naif = new Naif();
 	DefaultTeam welzl = new DefaultTeam();
 	long temps1, temps2;
-	FileWriter writer = new FileWriter(result, true);
-
-	for (int i = 10; i <= 1600; i++) {
+	FileWriter writer_result = new FileWriter(result, true);
+	FileWriter writer_csv = new FileWriter("Histogramme.csv");
+//	writer_csv.write("type, time_nanosec\n");
+	long totale_welzl=0 , totale_naif=0 ; 
+	for (int i = 2; i <= 200; i++) {
 		//List de points de chaque fichier
 		ArrayList<Point> points = new ArrayList<Point>();
 		try {
@@ -37,20 +39,27 @@ public class test {
 			temps1 = System.nanoTime();
 			naif.calculCercleMin(points);
 			temps1 =System.nanoTime() - temps1;
-
+			totale_naif =totale_naif+temps1;
+			
 			temps2 = System.nanoTime();
 			welzl.calculCercleMin(points);
 			temps2 =System.nanoTime() - temps2;
-
+			totale_welzl= temps2+totale_welzl;
+			
 			//affichage du resultat en nanosecond 
 			String contents = "test" + i + " Naif: " +( temps1) + " Welzl: " + (temps2) + "\n";
-			writer.write(contents);
-			writer.flush();
+			writer_result.write(contents);
+			writer_result.flush();
 			reader.close();
 		} catch (IOException e) {
 		}
 	}
-	writer.close();
+	writer_result.close();
+	writer_csv.write("Wzel "+ totale_welzl/200+"\n" );
+	
+	writer_csv.write("Naif, "+ totale_naif/200 );
+	writer_csv.close(); 
+
 }
 
 
